073473332bfb2296a1eb959c884d409cce7bcace
Jake Ryan <jryan6492@gmail.com>
2026-02-10 16:42:51 -0500

    fix load animation for 404

--- app/not-found.js
+++ app/not-found.js
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return (
-    <div className="flex flex-col flex-nowrap justify-center items-start size-full">
+    <div className="flex load-1 flex-col flex-nowrap justify-center items-start size-full">
       <h2 className="color-cream">404</h2>
       <h3 className="color-cream">You made an oopsie.</h3>
       <a className="btn mt-4" href="/"><span>Go Home</span></a>
