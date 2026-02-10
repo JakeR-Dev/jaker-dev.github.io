3a60ffb8912824062a1d3e30aaeb78cd0cfaad58
Jake Ryan <jryan6492@gmail.com>
2026-02-10 16:41:11 -0500

    updated resume and link for /about page

--- app/about/page.js
+++ app/about/page.js
@@ -15,7 +15,7 @@ export default function About() {
         <h2 className="color-cream">About Me</h2>
         <p className="color-cream">I'm a web developer with over a decade of experience in bringing innovative designs to life. I take the phrase "pixel perfect" literally and I believe that website interactions, when done right, can leave a lasting impact on the user.</p>
         <p className="color-cream">I have experience in a variety of technologies, including JavaScript, PHP, React, APIs, HTML, SCSS, Tailwind, and more. I have a deep knowledge of CMS platforms, most prominently WordPress, but also WebFlow and HubSpot. I have a strong understanding of ADA compliance and Google Analytics as well. Take a look at my resume to see my full skillset.</p>
-        <a href="/assets/img/Jake Ryan Resume - FS.pdf" className="btn" target="_blank"><span>Full Resume</span></a>
+        <a href="/Jake-Ryan-Resume-FS-FE(WP).pdf" className="btn" target="_blank"><span>Full Resume</span></a>
       </div>
       {/* testimonial */}
       <div className="right load-2 flex-[100%] md:flex-1">
